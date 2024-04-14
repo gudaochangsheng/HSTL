e243b10c60fa791703051798c5d8ea59841e2b29
Lei Wang <55449621+gudaochangsheng@users.noreply.github.com>
2024-04-14 20:40:03 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
 # For CASIA-B
 CUDA_VISIBLE_DEVICES=0, 1 python -m torch.distributed.launch --master_port XXXXX --nproc_per_node=2 lib/main.py --cfgs ./config/hstl.yaml --log_to_file --phase train
 # For OUMVLP
-#CUDA_VISIBLE_DEVICES=0, 1, 2, 3 python -m torch.distributed.launch --master_port XXXXX --nproc_per_node=2 lib/main.py --cfgs ./config/hstl_OUMVLP.yaml --log_to_file --phase train
+#CUDA_VISIBLE_DEVICES=0, 1, 2, 3 python -m torch.distributed.launch --master_port XXXXX --nproc_per_node=4 lib/main.py --cfgs ./config/hstl_OUMVLP.yaml --log_to_file --phase train
+#CUDA_VISIBLE_DEVICES=0, 1, 2, 3 python -m torch.distributed.launch --master_port XXXXX --nproc_per_node=4 lib/main.py --cfgs ./config/hstl_gait3d.yaml --log_to_file --phase train
+
